73e0cd41a87fa9fc45b248cf9c6f7c1d3441ae08
Birgitt Thomsen <birgitt.thomsen@t-online.de>
2026-07-12 11:57:26 +0200

    Updating table structure

--- models.py
+++ models.py
@@ -6,23 +6,26 @@ db = SQLAlchemy()
 
 class Feedback(db.Model):
     """ Defines columns for feedback table. """
-
     id = db.Column(db.Integer, primary_key=True)
     customer_name = db.Column(db.String(100), nullable=False)
     customer_id = db.Column(db.String(20), nullable=False)
     order_number = db.Column(db.String(25), nullable=False)
     comment = db.Column(db.Text, nullable=False)
     survey_type = db.Column(db.String(20), nullable=False)
-    survey_score = db.Column(db.Integer)
+    score = db.Column(db.Integer, nullable=False)
     source = db.Column(db.String(50))
 
+    feedback_date = db.Column(
+        db.DateTime,
+        nullable=False
+    )
     # Datetime set by system
     uploaded_at = db.Column(
         db.DateTime,
-        default=datetime.utcnow
+        default=datetime.utcnow,
+        nullable=False
     )
-
-    # Link to Analysis
+    # One-to-many as feedback may have analysis versions
     analyses = db.relationship(
         "Analysis",
         backref="feedback",
@@ -32,19 +35,29 @@ class Feedback(db.Model):
 
 class Analysis(db.Model):
     """ Defines columns for analysis table. """
-
     id = db.Column(db.Integer, primary_key=True)
     sentiment = db.Column(db.String(20), nullable=False)
     emotion = db.Column(db.String(30))
     themes = db.Column(db.JSON)
     priority = db.Column(db.String(20))
     confidence = db.Column(db.Float)
-    insight = db.Column(db.Text)
+    summary = db.Column(db.Text)
     analysis_json = db.Column(db.JSON)
+
+    # Datetime set by system
     analyzed_at = db.Column(
         db.DateTime,
-        default=datetime.utcnow
+        default=datetime.utcnow,
+        nullable=False
     )
+
+    #Captures version in case of re-analysis action
+    analysis_version = db.Column(
+        db.Integer,
+        default=1,
+        nullable=False
+    )
+
     # Link to feedback table
     feedback_id = db.Column(
         db.Integer,
